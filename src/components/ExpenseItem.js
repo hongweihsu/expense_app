077519beac944049d883baa9e20bb12a0fb7e355
hongweihsu <hongweih@student.unimelb.edu.au>
2022-02-24 18:25:58 +1100

    add more expense components and use 'props' argument

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   const expenseDate = new Date(2021, 2, 28);
   const expenseTitle = "Car Insurance";
   const expenseAmout = 294.67;
@@ -8,10 +8,10 @@ function ExpenseItem() {
   // return 一定只能回一個tag
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmout}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
